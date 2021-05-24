9e965b797966d6bdced630d48600dbe68e008dac
LDH <arriety93@gmail.com>
2021-05-24 17:36:54 +0900

    Second Commit

---
+++ circle_with_var.py
@@ -0,0 +1,11 @@
+radius=40.0
+print("원의 반지름: ", radius)
+print("원의 면적: ", 3.14*radius*radius)
+print("원의 둘레: ", 2*3.14*radius)
+
+name = "홍길동"
+age = 27
+print('안녕! 나는', name, '이야. 나는 나이가', age, '살이야.')
+print("안녕! 나는 %s 이야. 나는 나이가 %d 살이야." %(name,age))
+print("안녕! 나는 {0} 이야. 나는 나이가 {1} 살이야." .format(name,age))
+print(f'안녕! 나는 {name} 이야. 나는 나이가 {age} 살이야.')
