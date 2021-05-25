0422070284c6401aa05d789a8c58b8a1188e4d98
LDH <arriety93@gmail.com>
2021-05-25 12:35:25 +0900

    Second Commit

---
+++ if_01.py
@@ -0,0 +1,13 @@
+# 나이 = 19
+# if 나이 < 20 :
+#     print('청소년 할인')
+
+# walk = 1200
+# if walk >= 1000 :
+#     print('목표 달성')
+
+now = 14
+if now < 12 :
+    print('오전입니다.')
+else :
+    print('오후입니다.')
---
+++ seq_test.py
@@ -0,0 +1,6 @@
+num = 100
+print('num =', num)  #100이 출력됨
+num = num + 100
+print('num =', num)  #200이 출력됨
+num = num + 100 
+print('num =', num)  #300이 출력됨`
